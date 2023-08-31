ff372ee8505cea718729cb56503003143089a2cf
Duhan Kosali <71566458+duhankosali@users.noreply.github.com>
2023-08-31 09:43:28 +0300

    SignalR
    Server ve Client uygulamalarının geliştirilmesi, Otomatik bağlantı konfigürasyonu

---
+++ SignalR-Server/SignalR-Server/Hubs/MyHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR; // signalR namespace'i
+using System.Threading.Tasks;
+
+namespace SignalR_Server.Hubs
+{
+    public class MyHub : Hub
+    {
+        // Buraya POST işlemi yapıyorum.
+        public async Task SendMessageAsync(string message)
+        {
+            await Clients.All.SendAsync("receiveMessage", message);
+        }
+    }
+}
---
+++ SignalR-Server/SignalR-Server/Startup.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SignalR_Server.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR_Server
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // default course policy
+            services.AddCors(options => options.AddDefaultPolicy(policy =>
+                policy.AllowAnyMethod()
+                      .AllowAnyHeader()
+                      .AllowCredentials()
+                      .SetIsOriginAllowed(origin => true)
+                      
+            ));
+
+            // indicate signalR
+            services.AddSignalR(); 
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(); // using default cors policy
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<MyHub>("/myHub"); // endpoints
+            });
+        }
+    }
+}
